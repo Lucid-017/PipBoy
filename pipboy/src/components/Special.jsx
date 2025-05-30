f6f26e520691947c04b80510a838e819dd443072
Uchenna Onwuliri <uchennaonwuliri@gmail.com>
2025-05-30 08:26:19 +0100

    done with special tab

--- pipboy/src/components/Special.jsx
+++ pipboy/src/components/Special.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import "./css/Special.css";
+import Data from "./json/Special.json";
 
 const Special = () => {
+  const [hoveredImage,setHoveredImage] = useState();
+
+  const handleHoveredImage = (atrri) =>{
+    setHoveredImage(atrri)
+    console.log(atrri)
+  }
+  /**for each of the attributes, display names to the left and on hover display the image to the div at the right */
   return (
     <div>
-      special
-    </div>
-  )
-}
+        <div>
+          <div class="item-list  grid grid-cols-1 sm:grid-cols-2 place-items-start">
+            <div className="div1">
+              {Data.attributes.map((attri, index) => (
+                <div className="flex item mb-4">
+                  <p onMouseEnter={()=>handleHoveredImage(attri)} key={index} class="strength">{attri.name}</p>
+                  <p>{attri.score}</p>
+                </div>
+              ))}
+            </div>
+            <div className="div2">
+              <div className="rw">
+                  <div>
+                    {/* if image is attribute is hovered on */}
+                    {hoveredImage && (
+                      <div>
+                                              <img src={hoveredImage.image} alt={hoveredImage.name} className="w-[300px] h-[300px] mx-auto " />
+                        <div className="desc">
+                          <p>{hoveredImage.description}</p>
+                        </div>
 
-export default Special
+                      </div>
+                    )}
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Special;
--- pipboy/src/pages/Home.jsx
+++ pipboy/src/pages/Home.jsx
@@ -8,12 +8,12 @@ const Home = () => {
     const [isActive,setIsActive] = useState('STATUS')
     const tabs = [
         { name: "STATUS", path: "/status" },
-        { name: "SPECIAL", path: "/inventory" },
+        { name: "S.P.E.C.I.A.L", path: "/inventory" },
         { name: "PERKS", path: "/data" },
       ];
   return (
     <section className='px-5 md:px-10'>
-        <ul className='flex flex-wrap text-center sm:text-start items-center space-x-4 sm:px-8 md:pt-10'>
+        <ul className='flex flex-wrap text-center sm:text-start items-center space-x-4 sm:px-8 py-4 md:pt-10'>
         {tabs.map((item) => (
             <li onClick={()=>setIsActive(item.name)} key={item.name} className={`text-[20px] font-semibold tab-item cursor-pointer ${isActive ===item.name ? '':'opacity-[.5]'}`}>
                 <p>{item.name}</p>
@@ -21,7 +21,7 @@ const Home = () => {
         ))}
         </ul>
 
-        {isActive === 'STATUS' ? <Status/> : isActive=== 'SPECIAL' ? <Special/> : <Perks/>}
+        {isActive === 'STATUS' ? <Status/> : isActive=== 'S.P.E.C.I.A.L' ? <Special/> : <Perks/>}
                     
     </section>
   )
